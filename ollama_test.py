a2e7330c72a9052e69593df7274b76d637516c82
vidhanrathore <rathorevidhan2001@gmail.com>
2026-05-02 21:52:51 +0530

    Tested ollama in local with generate api.

---
+++ ollama_test.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+url = "http://localhost:11434/api/generate"
+
+data = {
+    "model": "mistral",
+    "prompt": "what is ollama",
+    "stream": False
+}
+
+response = requests.post(url, json=data)
+
+result = response.json()
+
+print(result["response"])
